766e7dbd327f5c8e00019b66db0b7a2e5faed7b1
unknown <Admin@DDM_14.utvmdc.mx>
2023-01-18 11:08:41 -0600

    Comandos git IONIC

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Console } from 'console';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page  implements OnInit {
+
+  constructor() {}
+  ngOnInit (): void {
+    
+    console.log ("Hola mundo en IONIC");
+    let numero =20;
+    debugger;
+    numero =10;
+    numero =15;
+    numero =100;
+    console.log(numero)
+
+    console.log("La variable esta tomando este valor: "+numero);
+
+
+  }
+
+}
